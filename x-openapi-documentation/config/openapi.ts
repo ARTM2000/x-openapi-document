577cec024aec8481994e291638744bfb592c8619
A.Tanoumandian <tanoomandian204@gmail.com>
2022-12-09 12:34:29 +0330

    chore: cleaned

--- x-openapi-documentation/config/openapi.ts
+++ x-openapi-documentation/config/openapi.ts
@@ -24,11 +24,7 @@ export const openapiSourceList: OpenApiSource[] = [
   {
     url: 'http://openapi-basic-math:1000/assets/swagger.json',
     prefix: '/math',
-  },
-  {
-    url: 'https://auth.staging.finnotech.ir/dezhbaan-swagger-json',
-    prefix: '/auth',
-  },
+  }
 ];
 
 /**
